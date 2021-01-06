a504f88074eb1cf3f13e6823c2f37ee54796e717
matteoferla <matteo.ferla@gmail.com>
2021-01-06 20:30:09 +0000

    :beetle: debug on prod server :cool:

--- homesensing/homesensing/utilities/gifify.py
+++ homesensing/homesensing/utilities/gifify.py
@@ -7,15 +7,14 @@ import logging
 log = logging.getLogger(__name__)
 
 folder = os.path.split(__file__)[0]
-parent = os.path.split(folder)[0]
-target_folder = os.path.join(parent, 'static')
+root_folder = os.path.split(folder)[0]
 
 def gifify(sensor_forename, photo_entries):
     size = 1080 / 2, 1920 / 2  # x/5 each
     images = []
     font = ImageFont.truetype("Vera.ttf", 32)
     for entry in photo_entries:
-        path = os.path.join(target_folder, entry['path'])
+        path = os.path.join(root_folder, entry['path'])
         print(path)
         if not os.path.exists(path):  # image removed (dick pic photobomb)
             log.warning(f'Cannot find file {entry["path"]}')
@@ -25,8 +24,8 @@ def gifify(sensor_forename, photo_entries):
         draw.text((0, 0), entry['datetime'], font=font, fill='cyan')
         im.thumbnail(size, Image.ANTIALIAS)
         images.append(im)
-    first_filename = os.path.join(target_folder, 'test.gif')
+    first_filename = os.path.join(root_folder, 'static', 'test.gif')
     imageio.mimsave(first_filename, images, fps=5)
-    final_filename = os.path.join(target_folder, f'{sensor_forename}.gif')
+    final_filename = os.path.join(root_folder, 'static', f'{sensor_forename}.gif')
     pygifsicle.optimize(first_filename, final_filename)
     return final_filename
